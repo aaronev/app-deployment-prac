b38c9db9cd95884a4edc45259ac748ad14aba7b6
aaronev <aarone.villanueva@gmail.com>
2017-08-09 15:09:12 -0700

    adds all dependencies and sets up sever.js

--- server.js
+++ server.js
@@ -1,9 +1,38 @@
 const express = require('express')
+const passport = require('./config/authentication')
+const session = require('express-session')
+const bodyParser = require('body-parser')
+const flash = require('connect-flash')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('hello')
+require('ejs')
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(session({secret: 'secret'}))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.userSess = req.user
+  res.locals.errorSignUp = req.flash('errorSignUp')
+  res.locals.errorLogin = req.flash('errorLogin')
+  next()
 })
 
-const port = 3000
-app.listen(port, console.log(`localhost:${port}`))
+app.use('/', require('./routers'))
+
+app.use((err, req, res, next) => {
+  res.status(500).render('./errors/error', {error: err})
+})
+
+app.use((req, res) => { 
+  res.render('./errors/not-found') 
+})
+
+const port = process.env.PORT || 3001
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}...`)
+})
